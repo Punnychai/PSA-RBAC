7eb121823174245a8803f9e112f60adfdda5d08a
Punnychai <rpunnachai@gmail.com>
2024-05-27 12:43:11 +0700

    Upload Resources

---
+++ Pages/About.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>About</title>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap');
+
+            body {
+                font-family: 'Inter', sans-serif;
+                margin: 0;
+                padding: 0;
+            }
+            pre {
+                font-family: 'Inter', sans-serif;
+                font-size: 22px;
+                font-weight: 500;
+                text-align: center;
+                line-height: 32px;
+                margin: 200px 0 0 -120px;
+                padding: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <pre>
+            This web application is created as a part of "Password Analyser with Role-Based Access Control" project.
+            It is aimed to improve users' password strength using Generative Artificial Intelligence
+            and visualise Role-Based Access Control policy using Relational Database Systems - MySQL.
+
+            About the Creator :
+            Punnachai Rodkaew
+            Computer Engineering (CyberSecurity and Cloud Computing) student at 
+            Sirindhorn International Institute of Technology (SIIT), Thammasat University.
+
+            If you can't find me at my desk, I'm probably grabbing a cup of coffee and playing with cats on the way back.
+        </pre>
+    </body>
